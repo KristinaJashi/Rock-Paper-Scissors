80328f182262a7f524e6b4b5aa1ad09e9019d388
Kristina Jashi <155985200+KristinaJashi@users.noreply.github.com>
2024-02-26 17:47:56 +0400

    Add files via upload

---
+++ rock_paper_scissors_game.js
@@ -0,0 +1,33 @@
+const playerMove = prompt(
+  "Enter your move: rock, paper, or scissors"
+).toLowerCase();
+
+const computerMove = ["rock", "paper", "scissors"][
+  Math.floor(Math.random() * 3)
+];
+
+let result = "";
+
+if (
+  (playerMove === "rock" && computerMove === "scissors") ||
+  (playerMove === "paper" && computerMove === "rock") ||
+  (playerMove === "scissors" && computerMove === "paper")
+) {
+  result = "You win!";
+} else if (playerMove === computerMove) {
+  result = "It's a tie!";
+} else {
+  result = "You lose!";
+}
+
+console.log("Your move: " + playerMove);
+console.log("Computer's move: " + computerMove);
+alert(
+  "You chose: " +
+    playerMove +
+    ".\n" +
+    "Computer chose : " +
+    computerMove +
+    ".\n" +
+    result
+);
